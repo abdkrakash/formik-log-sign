0f3f56c797122796563eec56f85be4259efcc3ed
abdkrakash <ABDKARKASH@GMAIL.COM>
2025-01-26 16:55:53 +0300

    done

---
+++ New folder/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './components/login'
+import Signup from './components/signup'
+
+
+function App() {
+  return (
+    <div>
+      <Login />
+      <Signup />
+    </div>
+  )
+}
+
+export default App
